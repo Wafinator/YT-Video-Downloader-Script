b39f99de53c40811aaed1b3650a14d84f9fed297
Wafi Hassan <65298130+Wafinator@users.noreply.github.com>
2024-01-15 18:10:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from pytube import YouTube
+
+
+try:
+    # User inserts Youtube link
+    url = input("Enter the YouTube URL: ")
+    
+    yt = YouTube(url)
+    
+    print("Title:", yt.title)
+    print("Views:", yt.views)
+
+    # Get the highest resolution stream
+    yd = yt.streams.get_highest_resolution()
+    
+    # Download the video to the current directory
+    yd.download()
+    
+    print("Download complete.")
+except Exception as e:
+    print("An error occurred:", str(e))
